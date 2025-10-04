b803436f3950e8b1e12a7625fa0c09ee445c3980
Abhinav Yadav <abhinav.20233011@mnnit.ac.in>
2025-10-04 22:34:53 +0530

    added more routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,38 @@
-import { useState } from 'react'
-import Login from './components/auth/Login'
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { AuthProvider } from "./contexts/authContext";
+import { ThemeProvider } from "./contexts/themeContext/themeContext";
+import Login from "./components/auth/Login";
+import UserRegister from "./components/auth/RegisterUser";
+import OfficerRegister from "./components/auth/OfficerRegister";
+import Admin from "./components/auth/Admin";
+import Home from "./components/home/home";
+import PrivateRoute from "./routes/PrivateRoute";
+import LandingPage from "./components/home/LandingPage";
 
 function App() {
-  
-
-  return(
-    <>
-      <h1>Hello World</h1>
-      <Login />
-    </>
-  )
+  return (
+    <AuthProvider>
+      <ThemeProvider>
+        <Router>
+          <Routes>
+            <Route path="/" element={<LandingPage />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<UserRegister />} />
+            <Route path="/signup-admin" element={<Admin />} />
+            <Route path="/signup-officer" element={<OfficerRegister />} />
+            <Route
+              path="/home"
+              element={
+                <PrivateRoute>
+                  <Home />
+                </PrivateRoute>
+              }
+            />
+          </Routes>
+        </Router>
+      </ThemeProvider>
+    </AuthProvider>
+  );
 }
 
-export default App
+export default App;
