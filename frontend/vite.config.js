32b92540907ad5ec39d4ad23f369f9d1936e16aa
Abhinav Yadav <abhinav.20233011@mnnit.ac.in>
2025-10-04 22:50:55 +0530

    update to add theme

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [tailwindcss(), react()],
+  
+  // ...
+  theme: {
+    extend: {
+      keyframes: {
+        pulseSlow: {
+          '0%, 100%': { transform: 'scale(1)' },
+          '50%': { transform: 'scale(1.02)' },
+        }
+      },
+      animation: {
+        'pulse-slow': 'pulseSlow 4s cubic-bezier(0.4, 0, 0.6, 1) infinite',
+      }
+    },
+  },
+  // ...
+
 })
