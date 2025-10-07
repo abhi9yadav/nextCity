9e9c7fdb5888753ceb03750ce9f8ab04e2d3d995
Adarsh Kumar <adarshkumar.mnnit@gmail.com>
2025-10-07 16:45:31 +0530

    chore(vite): use dotenv and process.env.PORT instead of hardcoded port

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import dotenv from "dotenv";
+
+dotenv.config({ path: "./.env" });
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(), react()],
@@ -22,7 +26,8 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:5000",
+        // target: "http://localhost:5000",
+        target: `http://localhost:${process.env.PORT || 5000}`,
         changeOrigin: true,
       },
     },
