d9c567c9d4d9c564a560bbd2108522cc71a8a6f0
Anujsharma88085 <anujkumarsharma88085@gmail.com>
2025-10-08 19:25:00 +0530

    Set up static files, JSON parsing, form handling, and global error handling in app.js

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,14 @@
 const dotenv = require("dotenv");
 const path = require("path");
+const rootDir = require('./utils/rootDir');
+
 dotenv.config({ path: path.resolve(__dirname, ".env") });
 
 const express = require("express");
 const cors = require("cors");
-const bodyParser = require("body-parser");
 
+const AppError = require("./utils/appError");
+const globalErrorHandler = require("./controllers/errorController");
 const userRoutes = require("./routes/authRoutes");
 const superAdminRoutes = require("./routes/superAdminRoutes");
 const complaintRoutes = require("./routes/complaintRoutes");
@@ -13,13 +16,23 @@ const complaintRoutes = require("./routes/complaintRoutes");
 const app = express();
 
 // Middlewares
+app.use(express.static(path.join(rootDir, 'public')));
 app.use(cors());
-app.use(bodyParser.json());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true })); // for form submissions
+app.set('json spaces', 2); // nicely formatted JSON responses
 
 // Routes
 console.log("Setting up routes...");
 app.use("/api/v1/users", userRoutes);
 app.use("/api/v1/superAdmin", superAdminRoutes);
 app.use("/api/v1/complaints", complaintRoutes);
+
+app.all(/.*/, (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
+
+// Global error handler
+app.use(globalErrorHandler);
 
 module.exports = app;
