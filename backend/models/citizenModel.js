a64d5dd32467bd69e509bbd2d27a93f119422011
Adarsh Kumar <adarshkumar.mnnit@gmail.com>
2025-10-09 16:27:31 +0530

    fixed bug in citizenModel.js

--- backend/models/citizenModel.js
+++ backend/models/citizenModel.js
@@ -1,10 +1,11 @@
-const User = require("./user");
+const mongoose = require("mongoose");
+const User = require("./userModel");
 
 const citizenSchema = new mongoose.Schema({
   address: { type: String },
   complaints: [{ type: mongoose.Schema.Types.ObjectId, ref: "Complaint" }]
 });
 
-const Citizen = User.discriminator("citizen", citizenSchema);
+const Citizen = User.discriminator("Citizen", citizenSchema);
 
 module.exports = Citizen;
