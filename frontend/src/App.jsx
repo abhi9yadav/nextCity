f11d8785ecc39405564d92ffd1a50d17a006fcac
Anujsharma88085 <anujkumarsharma88085@gmail.com>
2025-10-17 20:06:51 +0530

    Added Routes for deptAdmin

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,13 @@ import {
   useLocation,
 } from "react-router-dom";
 import { AuthProvider, useAuth } from "./contexts/authContext";
+import { useNavigate } from "react-router-dom";
+import { setNavigate } from "./utils/navigateHelper";
+import React from "react";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import Modal from "react-modal";
+Modal.setAppElement("#root");
 
 import RoleBasedRoute from "./routes/RoleBasedRoute";
 import Unauthorized from "./components/auth/Unauthorized";
@@ -26,20 +33,32 @@ import ManageZones from "./components/zones/ManageZones";
 import CreateZone from "./components/zones/CreateZone";
 import UpdateZone from "./components/zones/UpdateZone";
 import DeleteZone from "./components/zones/DeleteZone";
-import DashboardPage from './pages/deptAdmin/DashboardPage';
 import GetZones from "./components/zones/GetZones";
 import CityAdminDashboard from "./pages/CityAdminDashboard";
 import CityDepartmentComplaints from "./components/cityAdmin/CityDepartmentComplaints";
 import ManageDepartmentAdmins from "./components/cityAdmin/ManageDepartmentAdmins";
 import ManageDepartment from "./components/cityAdmin/ManageDepartment";
 import CreateCityPage from "./components/superAdmin/CreateCityPage";
+import DashboardPage from './pages/deptAdmin/DashboardPage';
+import WorkersLandingPage from './pages/deptAdmin/WorkersLandingPage';
+import AllWorkersListPage from './pages/deptAdmin/AllWorkersListPage';
+import AddWorkerPage from './pages/deptAdmin/AddWorkerPage';
+import ComplaintsPage from "./pages/deptAdmin/ComplaintsPage";
+import AnalyticsPage from "./pages/deptAdmin/AnalyticsPage";
+import WorkerDetailWrapper from "./pages/deptAdmin/WorkerDetailWrapper";
 
 import MainLayout from "./components/layout/MainLayout";
 import LoadingAnimation from "./components/loadingAnimation/LoadingAnimation";
 import VideoBackground from "./components/home/VideoBackground";
+
+
 function AppContent() {
   const { loading } = useAuth();
+  const navigate = useNavigate();
   const location = useLocation();
+  React.useEffect(() => {
+    setNavigate(navigate);
+  }, [navigate]);
 
   if (loading) return <FlowerAnimation />;
 
@@ -53,19 +72,10 @@ function AppContent() {
       {/* <Route path="/unauthorized" element={<Unauthorized />} /> */}
       <Route path="/loading" element={<VideoBackground />} />
       <Route path="/super-admin/create-city" element={<CreateCityPage />} />
-      <Route
-        path="/flower-animation"
-        element={
-          <MainLayout>
-            <FlowerAnimation />
-          </MainLayout>
-        }
-      />
-      
-      
-
-
-
+      <Route path="/" element={<MainLayout />}>
+          <Route path="flower-animation" element={<FlowerAnimation />} />
+        </Route>
+    
       <Route path="/set-password" element={<SetPassword />}/>
       {/* <Route path="/notfound" element={<Animated404Page />} /> */}
 
@@ -103,8 +113,17 @@ function AppContent() {
 
       {/* Department Admin */}
       <Route element={<RoleBasedRoute allowedRoles={["dept_admin"]} />}>
-      {/* <Route path="/dept-admin" element={<MainLayout />}> */}
-      <Route path="/dept-admin/dashboard" element={<DashboardPage />} />
+        <Route path="/dept-admin" element={<MainLayout />}> 
+          <Route index element={<Navigate to="dashboard" replace />} />
+          <Route path="dashboard" element={<DashboardPage />} />
+          <Route path="workers" element={<WorkersLandingPage />} />
+          <Route path="workers/list" element={<AllWorkersListPage />} />
+          <Route path="workers/add" element={<AddWorkerPage />} />
+          <Route path="complaints" element={<ComplaintsPage />} />
+          <Route path="analytics" element={<AnalyticsPage />} />
+          <Route path="profile" element={<Profile />} />
+          <Route path="/dept-admin/worker/:id" element={<WorkerDetailWrapper />} />
+        </Route>
       </Route>
 
       {/* Worker */}
@@ -123,7 +142,7 @@ function AppContent() {
       </Route>
 
       {/* ---------- CATCH-ALL ---------- */}
-      <Route path="*" element={<Navigate to="/notfound" />} />
+      <Route path="*" element={<Navigate to="/flower-animation" />} />
     </Routes>
   );
 }
@@ -133,6 +152,7 @@ function App() {
     <AuthProvider>
       <Router>
         <AppContent />
+        <ToastContainer position="top-right" autoClose={3000} hideProgressBar={false} newestOnTop closeOnClick rtl={false} pauseOnFocusLoss draggable pauseOnHover theme='colored'/>
       </Router>
     </AuthProvider>
   );
